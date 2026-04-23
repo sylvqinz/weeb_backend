78bd4a62e6d33813e647c810bb785ae184a9c997
Guillaume Amselle Andreux <guillaume@andreux.fr>
2026-04-23 14:30:54 +0200

    feat: add users application with customUser and customUserManager

---
+++ users/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+# --- 1. LE MANAGER ---
+class CustomUserManager(BaseUserManager):
+    """
+    Gestionnaire personnalisé où l'email est l'identifiant unique
+    pour l'authentification, à la place du nom d'utilisateur.
+    """
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("L'adresse email doit être renseignée."))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password) # Hache le mot de passe de façon sécurisée
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Le SuperUtilisateur doit avoir is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Le SuperUtilisateur doit avoir is_superuser=True.'))
+
+        return self.create_user(email, password, **extra_fields)
+
+# --- 2. LE MODÈLE USER ---
+class CustomUser(AbstractUser):
+    # On supprime définitivement le champ username hérité
+    username = None
+
+    # L'email reste obligatoire et unique
+    email = models.EmailField(_('adresse email'), unique=True, blank=False)
+
+    # On indique à Django que l'email est le nouvel identifiant de connexion
+    USERNAME_FIELD = 'email'
+
+    # first_name et last_name → hérités
+    # is_active              → hérité
+    # date_joined            → hérité (équivalent de created_at)
+
+    # REQUIRED_FIELDS est vide car l'email et le mot de passe sont requis par défaut
+    REQUIRED_FIELDS = []
+
+    # On lie notre modèle à notre nouveau gestionnaire
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- weeb_backend/settings.py
+++ weeb_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=s4&endsmr@hb7j@v)^*1dvl(a9s@@2c2i1wjt^d2@dy=iyt-b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Lisez la variable DEBUG. Le `default=False` s'assure qu'en production
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.CustomUser'
