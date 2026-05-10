504bbde23f5845e8fdf55a0f61c9e75c7d9263f3
Guillaume Amselle Andreux <guillaume@andreux.fr>
2026-05-10 21:36:18 +0200

    feat: add register and login paths

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.views import CustomTokenObtainPairView
+from users.views import CustomTokenObtainPairView, RegisterView
 
 urlpatterns = [
-    path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Sign Up
+    path('register/', RegisterView.as_view(), name='register'),
+
+    # Sign In
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+
+    # Refresh token
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
