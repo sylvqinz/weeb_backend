93cb2a30d79227c4763b8c16790c41391cfd9fbe
Guillaume Amselle Andreux <guillaume@andreux.fr>
2026-05-10 21:37:31 +0200

    fix: delete unused imports

--- weeb_backend/urls.py
+++ weeb_backend/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
-
-from users.views import MyTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
